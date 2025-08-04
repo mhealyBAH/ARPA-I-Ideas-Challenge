e43dddca50d00c90e272d8a06bc689f4cf35625d
mhealyBah <healy_michael@bah.com>
2025-08-04 09:21:51 -0400

    Initial setup done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc.
   base: "",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
