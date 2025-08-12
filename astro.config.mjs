fcc084496a2f913eb680a120b7520851b94fd55e
Bauguess, Shawn [USA] <Bauguess_Shawn@bah.com>
2025-08-12 07:43:29 -0400

    a few final updates for clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 // https://astro.build/config
 export default defineConfig({
   // If hosting on a subdomain, set the base here such as /itsjpo, /demo, etc.
-  base: "/itsjpo",
+  base: "/ideas-challenge",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
